acf58f3d5cdece98904a547feb33b4e820690d9f
Christopher.Wang <wangchao_ustb_2012@163.com>
2019-01-29 14:40:59 +0800

    Collaborative filtering cost function with regularization

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -46,11 +46,15 @@ J = sum(
             (X * Theta' - Y).*R
         ).^2
     )(:)
-) / 2;
+) / 2 + sum(
+    (Theta.^2)(:)
+) * (lambda / 2) + sum(
+    (X.^2)(:)
+) * (lambda / 2);
 
-X_grad = ((X * Theta' - Y).*R) * Theta;
+X_grad = (((X * Theta' - Y).*R) * Theta) + (lambda * X);
 
-Theta_grad = ((X * Theta' - Y).*R)' * X;
+Theta_grad = (((X * Theta' - Y).*R)' * X) + (lambda * Theta);
 
 % =============================================================
 
